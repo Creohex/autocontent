3fd036ef04cfe097c908d7e26c04ed680ba3f769
Creohex <paradise.source@gmail.com>
2023-04-26 15:15:15 +0300

    move video-related methods, add annotations

--- autocontent/utils.py
+++ autocontent/utils.py
@@ -1,21 +1,17 @@
-import json
 import re
-import time
 from datetime import datetime, timedelta
 from pathlib import Path
-
-from moviepy.editor import VideoFileClip
 
 
 TIME_FMT = r"%H:%M:%S"
 # TIME_FMT_MS = r"%H:%M:%S,%f"
+"""Time format strings."""
 
-FMT_MP4 = "mp4"
-FORMATS_VID = [FMT_MP4]
-"""Video formats."""
+ROOT_DIR = Path(__file__).absolute().parent.parent
+"""Project root directory."""
 
 
-def format_time_ms(seconds):
+def format_time_ms(seconds: float) -> str:
     """Formats seconds to HH:MM:SS:MS format."""
 
     formatted_td = str(timedelta(seconds=seconds)).split(".")[0]
@@ -26,7 +22,7 @@ def format_time_ms(seconds):
     return formatted_str
 
 
-def ensure_folder(file_path):
+def ensure_folder(file_path: Path | str) -> None:
     """Ensures that all folders leading to the provided file path exist."""
 
     if not isinstance(file_path, (str, Path)):
@@ -39,7 +35,7 @@ def ensure_folder(file_path):
             directory.mkdir()
 
 
-def check_existing_file(file, force=False):
+def check_existing_file(file: Path, force: bool | None = False) -> None:
     """Checks if file exists and deletes it when forced to."""
 
     if file.is_file() and file.exists():
@@ -49,7 +45,7 @@ def check_existing_file(file, force=False):
             raise Exception(f"file already exists: {file}")
 
 
-def write_to_file(file, contents):
+def write_to_file(file: Path | str, contents: str) -> None:
     """Writes contents to file.
 
     file (Path):
@@ -69,7 +65,7 @@ def write_to_file(file, contents):
         raise
 
 
-def parse_time_value(predicate):
+def parse_time_value(predicate: int | float | str) -> float:
     """Parses time input in various forms."""
 
     match predicate:
@@ -93,44 +89,3 @@ def parse_time_value(predicate):
             raise Exception(
                 f"Invalid time argument type ({predicate}, {type(predicate)})"
             )
-
-
-def check_video_extension(file):
-    """Checks if provided video file type is supported."""
-
-    return Path(file).suffix[1:] in FORMATS_VID
-
-
-def check_video_file(file) -> VideoFileClip:
-    """Checks if video file exists and in correct ."""
-
-    file = Path(file).absolute()
-    if not file.exists():
-        raise Exception("File not found")
-    if not check_video_extension(file):
-        raise Exception(f"Incorrect file format (supported: {','.join(FORMATS_VID)})")
-
-    return str(file)
-    # return VideoFileClip(str(s))
-
-
-def clip_video(source_file, t1, t2, target_file=None, strip_sound=False, force=False):
-    """Cut clip from a video file."""
-
-    t1 = parse_time_value(t1)
-    t2 = parse_time_value(t2)
-    if t1 >= t2:
-        raise Exception(f"Incorrect time range provided ({t1} - {t2})")
-
-    source_file = Path(source_file).absolute()
-    video_path = check_video_file(source_file)
-    target_file = source_file.parent / Path(
-        target_file or f"{source_file.stem}-clip-{str(t1)}-{str(t2)}.{FMT_MP4}"
-    )
-    check_existing_file(target_file, force=force)
-
-    with VideoFileClip(video_path) as vid:
-        subclip = vid.subclip(t1, t2)
-        if strip_sound:
-            subclip = subclip.without_audio()
-        subclip.write_videofile(str(target_file))
