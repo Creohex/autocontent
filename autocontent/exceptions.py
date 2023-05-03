ba644b96701e84a46ee27573de7729ade045c4ca
Creohex <paradise.source@gmail.com>
2023-05-03 20:44:20 +0300

    exceptions: add InvalidFilePath exception

--- autocontent/exceptions.py
+++ autocontent/exceptions.py
@@ -13,6 +13,11 @@ class InvalidFileName(Error):
         super().__init__(msg=msg or "Invalid file name")
 
 
+class InvalidFilePath(Error):
+    def __init__(self, msg: str = None) -> None:
+        super().__init__(msg=msg or "Invalid file path")
+
+
 class VideoException(Error):
     def __init__(self, msg: str = None) -> None:
         super().__init__(msg)
