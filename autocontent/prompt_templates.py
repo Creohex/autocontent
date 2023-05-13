502f01cad86cd72f1bf2bd1bf96aaa72b9e14e0b
Creohex <paradise.source@gmail.com>
2023-05-13 18:07:19 +0300

    prompts: update

--- autocontent/prompt_templates.py
+++ autocontent/prompt_templates.py
@@ -1,5 +1,9 @@
 
 
-SUMMARIZE_SINGLE = """\
-...
+VIDEO_TITLE_GENERATION = """\
+TEXT is a video file transcription.
+Generate {{title_count}} titles for provided TEXT.
+Output format must be a list of strings, for example: ["title1", "title2", "title3"]
+
+TEXT: {{context}}
 """
